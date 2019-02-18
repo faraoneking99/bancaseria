30375708c50cac2810c2f531e31f48736f9a2f79
Domenico <razorpsn7@gmail.com>
2019-02-18 15:12:53 +0100

    Update Utente.java

--- src/Utente.java
+++ src/Utente.java
@@ -1,15 +1,61 @@
+
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 class Utente 
 {
-	
 	private String nome;
 	private String cognome;
 	private LocalDate datanascita;
 	private String email;
 	private int PIN;
 	ArrayList <Conto> conti = new ArrayList<Conto>();
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+
+	public LocalDate getDatanascita() {
+		return datanascita;
+	}
+
+
+	public void setDatanascita(LocalDate datanascita) {
+		this.datanascita = datanascita;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public int getPIN() {
+		return PIN;
+	}
 	
 	
 	public Utente(String n,String c,LocalDate a,String s)
@@ -24,6 +70,3 @@ class Utente
 	}
 
 }
-
-	
-	
