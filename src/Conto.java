39135337420c9bd2c1479894f11502a6da3cbb89
Christian <christiangenn99@gmail.com>
2019-02-18 16:54:12 +0100

    abilitazione transazioni

--- src/Conto.java
+++ src/Conto.java
@@ -1,45 +1,48 @@
-class Conto 
-{	
-    private String nome;
-    private double saldo = 0.0;
+package BANCA;
 
-    public Conto(String n) {
-        setNome(n);
+import java.time.LocalDate;
 
-    }
+public class Conto {
+    double saldo = 0.0;
+    LocalDate  ultimaModifica;
+    double vers = 0.0;
+    double ritiro = 0.0;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void ritira(double quantita)
-    {
-        if(quantita<20)
-        {
-        System.out.println("Quantita' minore di 20E...");
-        return;
-        }
-        if(saldo-quantita < 0)
-            System.out.println("Non puoi ritirare! Saldo non sufficiente");
-        else
-            saldo -= quantita;
-
-    }
-    public double getSaldo()
-    {
-        return saldo;
-    }
-    
-    public void deposita(double quantita)
-    {
-        if(quantita < 50){
-        System.out.println("Non puoi depositare una somma minore di 50E...");return;
+    public void versamento(double vers){
+        if(ultimaModifica == null)
+            ultimaModifica = LocalDate.now();
+        else if (ultimaModifica != LocalDate.now()) {
+            vers = 0.0;
+            ultimaModifica = LocalDate.now();
         }
         else
-        saldo += saldo +quantita;
+        if(vers+this.vers <= 500) {
+            if(vers >= 50)
+                this.vers += vers;
+            else
+                System.out.println("Devi versare minimo 50$");
+        }
+        else
+            System.out.println("Puoi versare 500$ al giorno (versamento massimo rimanente  " + (500-this.vers) + "$" );
     }
+
+    public void ritiro(double ritiro){
+        if(ultimaModifica == null)
+            ultimaModifica = LocalDate.now();
+        else if (ultimaModifica != LocalDate.now()) {
+            ritiro = 0.0;
+            ultimaModifica = LocalDate.now();
+        }
+        else
+        if(ritiro+this.ritiro <= 500) {
+            if(ritiro >= 20)
+                this.ritiro += ritiro;
+            else
+                System.out.println("Devi ritirare minimo 20$");
+        }
+        else
+            System.out.println("Puoi ritirare 500$ al giorno (ritiro massimo rimanente  " + (500-this.vers) + "$" );
+    }
+
 }
+
